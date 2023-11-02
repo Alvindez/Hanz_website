ccf1880842de30d02556a91c36ac82b22d4cde6b
alvindez <dezwasswa4@gmail.com>
2023-11-02 17:26:11 +0600

    urls,views,settings worked on

--- HanzWebsite/ArtWork/views.py
+++ HanzWebsite/ArtWork/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def gallery(request):
+    return render(request, 'Artwork/gallery.html')
+
+def viewphoto(request, pk):
+    return render(request, 'Artwork/photo.html')
+
+def addphoto(request):
+    return render(request, 'Artwork/add.html')
+
+
